668caf434ff6f0670f0846399152084f313ccfef
David <46841392+David654@users.noreply.github.com>
2021-11-24 09:17:32 +0200

    Add files via upload

---
+++ Settings.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class Settings
+{
+    public static Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    public static int width = 1202;
+    public static int height = 802;
+    public static int tile_size = 50;
+
+    public static final String title = "Maze Generator";
+    public static final Color bg = new Color(70, 70, 70);
+    public static final Color bg2 = new Color(148, 201, 115);
+    public static final Color line_color = new Color(70, 70, 70);
+}
